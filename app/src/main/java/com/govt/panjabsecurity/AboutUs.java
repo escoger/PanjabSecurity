d60a9927f8498689a41a5ed64851cdb450088a5c
GAURAV KUMAR <gaurav.kumar8@microfocus.com>
2019-09-12 19:50:46 +0530

    about us

---
+++ app/src/main/java/com/govt/panjabsecurity/AboutUs.java
@@ -0,0 +1,14 @@
+package com.govt.panjabsecurity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AboutUs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
